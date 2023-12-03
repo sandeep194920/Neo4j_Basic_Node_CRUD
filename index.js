4ba07b7c518c7666ccfbc4cb98bb1e7bb4b8cbca
sandeep194920 <sandeepamaranath@gmail.com>
2023-12-03 15:22:03 -0500

    read and create nodes

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 import { config } from 'dotenv'
-import { setupConnection } from './playWithAura.js'
+import {
+  getUser,
+  setupConnection,
+  createUser,
+  createItem,
+} from './playWithAura.js'
 
 config()
 // Create Express instance
 export const { NEO4J_URI, NEO4J_USERNAME, NEO4J_PASSWORD, APP_PORT } =
   process.env
 
-setupConnection()
+export const driver = await setupConnection()
+
+// READ OPERATION - GET EXISTING USER
+// const existingUser = await getUser()
+// console.log('The read user is', existingUser)
+
+// WRITE OPERATION - CREATE A USER
+// const user = await createUser('john1', 'John', 30)
+// console.log('The users are', user)
+
+// WRITE OPERATION - CREATE AN ITEM
+const item = await createItem('chair')
+console.log('The item created is', item)
--- playWithAura.js
+++ playWithAura.js
@@ -1,6 +1,6 @@
 // In this file, we will read, create, update nodes on neo4j aura db
 
-import { NEO4J_PASSWORD, NEO4J_URI, NEO4J_USERNAME } from './index.js'
+import { NEO4J_PASSWORD, NEO4J_URI, NEO4J_USERNAME, driver } from './index.js'
 import neo4j from 'neo4j-driver'
 
 /* I'm following this guides here
@@ -20,7 +20,72 @@ export const setupConnection = async () => {
     const serverInfo = await driver.getServerInfo()
     console.log('Connection established')
     console.log(serverInfo)
+    return driver
   } catch (err) {
     console.log(`Connection error\n${err}\nCause: ${err.cause}`)
   }
+}
+
+/*READ OPERATION*/
+/* Now that the basic connection works, let's access a node that is created in Aura and print it here*/
+export const getUser = async () => {
+  const session = driver.session()
+  const res = await session.executeRead(async (tx) => {
+    return await tx.run(
+      `MATCH (user:User {name:$name})
+      RETURN user`,
+      { name: 'Susan' }
+    )
+  })
+
+  const [firstUser] = res.records
+  console.log('The first user is')
+  console.log(firstUser)
+  const { name } = firstUser?.get('user')?.properties || 'NOT_FOUND'
+
+  /* IF there are multiple records you want to have in an array then do the below. 
+  Generally you will take this approach if you have LIMIT at the end of query*/
+  // const names = res.records.map((record) => record.get('user.name'))
+  // console.log(names)
+
+  await session.close()
+  return name
+}
+
+/*WRITE OPERATION*/
+export const createUser = async (username, name, age) => {
+  const session = driver.session()
+  const res = await session.executeWrite(async (tx) => {
+    return await tx.run(
+      `MERGE (user:User {username: $username})
+      ON CREATE SET user.name = $name, user.age = $age
+      RETURN user.name as name, user.username as username, user.age as age`,
+      { username: username, name: name, age: age }
+    )
+  })
+  const users = res.records.map((record) => {
+    return {
+      username: record.get('username'),
+      name: record.get('name'),
+      age: record.get('age'),
+    }
+  })
+
+  return users
+}
+
+/*WRITE OPERATION - ITEM WITH RANDOM id*/
+export const createItem = async (name) => {
+  const session = driver.session()
+  const res = await session.executeWrite(async (tx) => {
+    return await tx.run(
+      `MERGE (item:Item {item_name: $name, item_id: randomUuid()})
+      RETURN item`,
+      { name }
+    )
+  })
+
+  const [firstItem] = res.records
+  const { item_name, item_id } = firstItem.get('item').properties
+  return { item_name, item_id }
 }
